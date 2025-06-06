b5766c6097942893a5f8842bb3f37f5ccee7a324
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-06-06 02:13:47 -0300

    fix css path

--- trabajo-practico-craftsy-mvc/src/app.js
+++ trabajo-practico-craftsy-mvc/src/app.js
@@ -4,21 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..', 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
